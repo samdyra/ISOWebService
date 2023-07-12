f39a7ac0826fcd1dedb934fdcbb8e489e2d8f93c
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-07-12 22:28:43 +0700

    temp changes

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 
 
 def startup_db_client(app, config):
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
+    app.mongodb_client = MongoClient('mongodb+srv://dwisam001:dwisam001@isocluster.wfghllw.mongodb.net/?retryWrites=true&w=majority')
+    app.database = app.mongodb_client[config["ISOAPPSERVICE"]]
 
 def shutdown_db_client(app):
     app.mongodb_client.close()
