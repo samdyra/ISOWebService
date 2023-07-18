c37f88b7b74021d63e8318758ec7d6214d9ef564
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-07-19 02:36:17 +0700

    chore: update cors

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from config.database import startup_db_client, shutdown_db_client
 from bookstore.routes import router as book_router
 from s100.s102.routes import router as s102_router
 from dotenv import dotenv_values
+from fastapi.middleware.cors import CORSMiddleware
 
 config = dotenv_values(".env")
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
