2ccefc163695f1e5247cffb64fbdbc4f715cb102
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-08-12 22:03:56 +0700

    feat: progress on s111 api development

--- products/S111_SurfaceCurrent/Create_S111_dcf2.py
+++ products/S111_SurfaceCurrent/Create_S111_dcf2.py
@@ -3,7 +3,7 @@ import numpy
 import h5py
 from osgeo import gdal, osr
 
-#input surface current raster dataset (dcf2)
+# input surface current raster dataset (dcf2)
 dataset_173 = gdal.Open("wgs84_173_final.tif")
 dataset_174 = gdal.Open("wgs84_174_final.tif")
 dataset_175 = gdal.Open("wgs84_175_final.tif")
@@ -12,22 +12,22 @@ dataset_175 = gdal.Open("wgs84_175_final.tif")
 deg_band_173 = dataset_173.GetRasterBand(1)
 deg_grid_173 = deg_band_173.ReadAsArray()
 
-mag_band_173 = dataset_173.GetRasterBand(2) 
-mag_grid_173 = mag_band_173.ReadAsArray() #todolist : konversi satuan dari mps > knots
+mag_band_173 = dataset_173.GetRasterBand(2)
+mag_grid_173 = mag_band_173.ReadAsArray()
 
 deg_band_174 = dataset_174.GetRasterBand(1)
 deg_grid_174 = deg_band_174.ReadAsArray()
 
-mag_band_174 = dataset_174.GetRasterBand(2) 
-mag_grid_174 = mag_band_174.ReadAsArray() 
+mag_band_174 = dataset_174.GetRasterBand(2)
+mag_grid_174 = mag_band_174.ReadAsArray()
 
 deg_band_175 = dataset_175.GetRasterBand(1)
 deg_grid_175 = deg_band_175.ReadAsArray()
 
-mag_band_175 = dataset_175.GetRasterBand(2) 
-mag_grid_175 = mag_band_175.ReadAsArray() 
+mag_band_175 = dataset_175.GetRasterBand(2)
+mag_grid_175 = mag_band_175.ReadAsArray()
 
-#input metadata
+# input metadata
 metadata = {
     'geographicIdentifier': 'Selat Alas',
     'epoch': 'G1762',
@@ -76,20 +76,26 @@ with h5py.File('111ID00ITBS100PRJCT_DCF8.h5', 'w') as f:
     # initiate dataset structure
     surf = f.create_group('/SurfaceCurrent')
     surf_01 = f.create_group('/SurfaceCurrent/SurfaceCurrent.01')
-    
-    surf_group_object_01 = surf_01.create_group('/SurfaceCurrent/SurfaceCurrent.01/Group_001')
-    grid_01 = surf_group_object_01.create_dataset('values', dtype=[('surfaceCurrentSpeed', '<f4'), ('surfaceCurrentDirection', '<f4')], shape=deg_grid_173.shape)
-    
-    surf_group_object_02 = surf_01.create_group('/SurfaceCurrent/SurfaceCurrent.01/Group_002')
-    grid_02 = surf_group_object_02.create_dataset('values', dtype=[('surfaceCurrentSpeed', '<f4'), ('surfaceCurrentDirection', '<f4')], shape=deg_grid_173.shape)
-    
-    surf_group_object_03 = surf_01.create_group('/SurfaceCurrent/SurfaceCurrent.01/Group_003')
-    grid_03 = surf_group_object_03.create_dataset('values', dtype=[('surfaceCurrentSpeed', '<f4'), ('surfaceCurrentDirection', '<f4')], shape=deg_grid_173.shape)
-    
+
+    surf_group_object_01 = surf_01.create_group(
+        '/SurfaceCurrent/SurfaceCurrent.01/Group_001')
+    grid_01 = surf_group_object_01.create_dataset('values', dtype=[(
+        'surfaceCurrentSpeed', '<f4'), ('surfaceCurrentDirection', '<f4')], shape=deg_grid_173.shape)
+
+    surf_group_object_02 = surf_01.create_group(
+        '/SurfaceCurrent/SurfaceCurrent.01/Group_002')
+    grid_02 = surf_group_object_02.create_dataset('values', dtype=[(
+        'surfaceCurrentSpeed', '<f4'), ('surfaceCurrentDirection', '<f4')], shape=deg_grid_173.shape)
+
+    surf_group_object_03 = surf_01.create_group(
+        '/SurfaceCurrent/SurfaceCurrent.01/Group_003')
+    grid_03 = surf_group_object_03.create_dataset('values', dtype=[(
+        'surfaceCurrentSpeed', '<f4'), ('surfaceCurrentDirection', '<f4')], shape=deg_grid_173.shape)
+
     Group_F = f.create_group('Group_F')
     Group_F = f['/Group_F']
 
-    #initiate root attrs.
+    # initiate root attrs.
     f.attrs['productSpecification'] = "INT.IHO.S-111.1.0.1"
     f.attrs['eastBoundLongitude'] = maxx
     f.attrs['westBoundLongitude'] = minx
@@ -117,7 +123,7 @@ with h5py.File('111ID00ITBS100PRJCT_DCF8.h5', 'w') as f:
         f.attrs['metadata'] = metadata.get("metadata", "")
     if "issueTime" in metadata:
         f.attrs['issueTime'] = metadata.get("issueTime", "")
-    
+
     # initiate surfCurrent attribute
     srs = osr.SpatialReference()
     srs.ImportFromEPSG(source_epsg)
@@ -129,24 +135,24 @@ with h5py.File('111ID00ITBS100PRJCT_DCF8.h5', 'w') as f:
         axes = ["Longitude", "Latitude"]
 
     axis_names = surf.create_dataset('axisNames', data=axes)
-    
+
     surf.attrs['verticalUncertainty'] = -1.0
     surf.attrs['timeUncertainty'] = -1.0
-    surf.attrs.create('sequencingRule.type',data =  1)
+    surf.attrs.create('sequencingRule.type', data=1)
     surf.attrs['numInstances'] = 1
     surf.attrs['horizontalPositionUncertainty'] = -1.0
     surf.attrs['dimension'] = 2
     surf.attrs['sequencingRule.scanDirection'] = ", ".join(axes)
-    surf.attrs.create('interpolationType', data = 1)
-    surf.attrs.create('dataCodingFormat', data = 2)
-    surf.attrs.create('commonPointRule', data = 2)
+    surf.attrs.create('interpolationType', data=1)
+    surf.attrs.create('dataCodingFormat', data=2)
+    surf.attrs.create('commonPointRule', data=2)
 
     # initiate BathCov.nn attribute
     surf_01.attrs['eastBoundLongitude'] = maxx
     surf_01.attrs['westBoundLongitude'] = minx
     surf_01.attrs['southBoundLatitude'] = miny
     surf_01.attrs['northBoundLatitude'] = maxy
-    
+
     surf_01.attrs['numberOfTimes'] = 3
     surf_01.attrs['timeRecordInterval'] = 3600
     surf_01.attrs['dateTimeOfFirstRecord'] = '2022-09-29 16:00:00Z'
@@ -169,7 +175,7 @@ with h5py.File('111ID00ITBS100PRJCT_DCF8.h5', 'w') as f:
 
     grid_01[:, 'surfaceCurrentDirection'] = deg_grid_173
     grid_01[:, 'surfaceCurrentSpeed'] = mag_grid_173
-    
+
     grid_02[:, 'surfaceCurrentDirection'] = deg_grid_174
     grid_03[:, 'surfaceCurrentSpeed'] = mag_grid_174
 
@@ -177,15 +183,16 @@ with h5py.File('111ID00ITBS100PRJCT_DCF8.h5', 'w') as f:
     grid_01[:, 'surfaceCurrentSpeed'] = mag_grid_175
 
     # fill Group_F
-    dt_dtype = h5py.special_dtype(vlen= str)
+    dt_dtype = h5py.special_dtype(vlen=str)
     dt = numpy.dtype([('code', dt_dtype), ('name', dt_dtype), ('uom.name', dt_dtype), ('fillValue', dt_dtype),
                       ('datatype', dt_dtype), ('lower', dt_dtype), ('upper', dt_dtype), ('closure', dt_dtype)])
 
-    scurrent = Group_F.create_dataset('SurfaceCurrent', shape= (2,), dtype= dt)
-        
-    scurrent[0] = ('surfaceCurrentSpeed', 'Surface current speed', 'knots', '-9999.0', 'H5T_FLOAT', '0.0', '', 'geSemiInterval')
-    scurrent[1] = ('surfaceCurrentDirection', 'Surface current direction', 'arc-degrees', '-9999.0', 'H5T_FLOAT', '-0.0', '360', 'geLtInterval')
-    
-    fcode = Group_F.create_dataset('featureCode', shape= (1,), dtype = dt_dtype)
-    fcode[0] = ('SurfaceCurrent')
+    scurrent = Group_F.create_dataset('SurfaceCurrent', shape=(2,), dtype=dt)
 
+    scurrent[0] = ('surfaceCurrentSpeed', 'Surface current speed',
+                   'knots', '-9999.0', 'H5T_FLOAT', '0.0', '', 'geSemiInterval')
+    scurrent[1] = ('surfaceCurrentDirection', 'Surface current direction',
+                   'arc-degrees', '-9999.0', 'H5T_FLOAT', '-0.0', '360', 'geLtInterval')
+
+    fcode = Group_F.create_dataset('featureCode', shape=(1,), dtype=dt_dtype)
+    fcode[0] = ('SurfaceCurrent')
--- s100/s111/routes.py
+++ s100/s111/routes.py
@@ -1,26 +1,74 @@
+from typing import List
 from fastapi import APIRouter, Body, Request, status, HTTPException, Response
+from fastapi.encoders import jsonable_encoder
+from s100.s111.models import S111Product, S111ProductResponse
+from config.firebase import storage
+from h5py import enum_dtype
+from s100.utils.base64_tiff import convert_base64_to_temp_tiff
+from s100.constants.metadata_dict import COMMON_POINT_RULE, DATA_CODING_FORMAT, INTERPOLATION_TYPE, SEQUENCING_RULE_TYPE, VERTICAL_DATUM
+import io
+from osgeo import gdal, osr
+import numpy
+from s100.utils.tiff_hdf5_s102 import tiff_hdf5_s102 as convert_tiff_to_hdf5_s102
+from s100.utils.tiff_geojson import convert_tiff_to_geojson
+from s100.utils.global_helper import generate_random_filename
+from uuid import uuid4
+
 
 router = APIRouter()
 
 
+@router.post("/", response_description="Create a new s111 hdf5 file", status_code=status.HTTP_201_CREATED, response_model=S111ProductResponse)
+def create_s111(request: Request, input: S111Product = Body(...)):
+    # get input from request body
 
-@router.get("/", response_description="Get S111")
-def list_user_s102_data( request: Request):
-    print("s111")
-    return "s111"
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"error")
+    metadata = input.metadata
+    format_data = input.format_data
 
-@router.post("/", response_description="Get S111")
-def list_user_s102_data( request: Request):
-    print("s111 post")
-    return "s111 post"
+    temp_tiff_1 = convert_base64_to_temp_tiff(input.dataset_1)
+    temp_tiff_2 = convert_base64_to_temp_tiff(input.dataset_2)
+    temp_tiff_3 = convert_base64_to_temp_tiff(input.dataset_3)
 
-@router.patch("/", response_description="Get S111")
-def list_user_s102_data( request: Request):
-    print("s111 patch")
-    return "s111 patch"
+    dataset_1 = gdal.Open(temp_tiff_1)
+    dataset_2 = gdal.Open(temp_tiff_2)
+    dataset_3 = gdal.Open(temp_tiff_3)
 
-@router.delete("/{id}", response_description="Get S111")
-def list_user_s102_data( id: str, request: Request):
-    print("s111 delete ")
-    return Response(content=f"s111 delete {id}")
+    deg_band_1 = dataset_1.GetRasterBand(1)
+    deg_grid_1 = deg_band_1.ReadAsArray()
+
+    mag_band_1 = dataset_1.GetRasterBand(2)
+    mag_grid_1 = mag_band_1.ReadAsArray()
+
+    deg_band_2 = dataset_2.GetRasterBand(1)
+    deg_grid_2 = deg_band_2.ReadAsArray()
+
+    mag_band_2 = dataset_2.GetRasterBand(2)
+    mag_grid_2 = mag_band_2.ReadAsArray()
+
+    deg_band_3 = dataset_3.GetRasterBand(1)
+    deg_grid_3 = deg_band_3.ReadAsArray()
+
+    mag_band_3 = dataset_3.GetRasterBand(2)
+    mag_grid_3 = mag_band_3.ReadAsArray()
+
+    ulx, dxx, dxy, uly, dyx, dyy = dataset_1.GetGeoTransform()
+
+    # generate id(s)
+    user_id = input.user_id
+    uid4 = uuid4()
+    uuid_str = str(uid4)
+
+    # insert new s111 to mongodb
+    new_s111 = request.app.database["s111"].insert_one({
+        "_id": uuid_str,
+        "hdf5Uri": "",
+        "geojsonUri": "",
+        "file_name": "",
+        "user_id": user_id,
+    })
+
+    created_s111 = request.app.database["s111"].find_one(
+        {"_id": new_s111.inserted_id}
+    )
+
+    return created_s111
