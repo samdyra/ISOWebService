ee4e8cbb7c9ef079448ada081d8aef713b7aeee1
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-08-18 00:17:10 +0700

    feat: adjust s104 routes

--- s100/s104/routes.py
+++ s100/s104/routes.py
@@ -5,7 +5,8 @@ from s100.utils.base64_tiff import convert_base64_to_temp_tiff
 from s100.constants.metadata_dict import COMMON_POINT_RULE, DATA_CODING_FORMAT, INTERPOLATION_TYPE, SEQUENCING_RULE_TYPE, VERTICAL_DATUM
 import io
 from osgeo import gdal, osr
-# from s100.utils.tiff_hdf5_s111 import tiff_hdf5_s111 as convert_tiff_to_hdf5_s111
+from s100.utils.tiff_hdf5_s104 import tiff_hdf5_s104 as convert_tiff_to_hdf5_s104
+from s100.utils.convert_base64_netcdf_tiff import save_raster_to_temp_file as convert_netcdf_to_temp_tiff
 from uuid import uuid4
 from fastapi.encoders import jsonable_encoder
 from s100.utils.global_helper import generate_random_filename
@@ -19,16 +20,92 @@ router = APIRouter()
 def create_s104(request: Request, input: S104Product = Body(...)):
     # get input from request body
     metadata = input.metadata
+    format_data = input.format_data
+
+    temp_tiff_deg = convert_netcdf_to_temp_tiff(
+        input.dataset_ncdf, 'Surface Current Direction')
+    temp_tiff_mag = convert_netcdf_to_temp_tiff(
+        input.dataset_ncdf, 'Surface Current Speed')
+
+    dataset_deg = gdal.Open(temp_tiff_deg)
+    dataset_mag = gdal.Open(temp_tiff_mag)
+
+    deg_band_1 = dataset_deg.GetRasterBand(1)
+    deg_grid_1 = deg_band_1.ReadAsArray()
+
+    # calculate grid origin and res.
+    # get six coefficients affine transformation
+    ulx, dxx, dxy, uly, dyx, dyy = dataset_deg.GetGeoTransform()
+
+    if "origin" not in metadata:
+        # shift the gdal geotransform corner point to reference the node (pixel is center) rather than cell (pixel is area)
+        metadata["origin"] = [ulx + dxx/2, uly + dyy/2]
+
+    if "res" not in metadata:
+        metadata["res"] = [dxx, dyy]
+
+    if "horizontalDatumReference" not in metadata or "horizontalDatumValue" not in metadata:
+        metadata["horizontalDatumReference"] = "EPSG"
+        epsg = osr.SpatialReference(
+            dataset_deg.GetProjection()).GetAttrValue("AUTHORITY", 1)
+        try:
+            metadata["horizontalDatumValue"] = int(epsg)
+        except TypeError:
+            if osr.SpatialReference(dataset_deg.GetProjection()).GetAttrValue("GEOGCS") == 'WGS 84':
+                metadata["horizontalDatumValue"] = 4326
+
+    file_name = generate_random_filename(metadata['file_name'])
+    res_x, res_y = metadata["res"]
+    rows, cols = deg_grid_1.shape
+    corner_x, corner_y = metadata['origin']
+
+    # S-111 is node based, so distance to far corner is res * (n -1)
+    opposite_corner_x = corner_x + res_x * (cols - 1)
+    opposite_corner_y = corner_y + res_y * (rows - 1)
+
+    minx = min((corner_x, opposite_corner_x))
+    maxx = max((corner_x, opposite_corner_x))
+    miny = min((corner_y, opposite_corner_y))
+    maxy = max((corner_y, opposite_corner_y))
+
+    data_coding_format_dt = enum_dtype(DATA_CODING_FORMAT, basetype='i4')
+    vertical_datum_dt = enum_dtype(VERTICAL_DATUM, basetype='i4')
+    common_point_rule_dt = enum_dtype(COMMON_POINT_RULE, basetype='i4')
+    interpolation_type_dt = enum_dtype(INTERPOLATION_TYPE, basetype='i4')
+    sequencing_rule_type_dt = enum_dtype(SEQUENCING_RULE_TYPE, basetype='i4')
+
+    # create hdf5 instance in memory
+    bio = io.BytesIO()
+    convert_tiff_to_hdf5_s104(bio, {
+        'dataset_deg': dataset_deg,
+        'dataset_mag': dataset_mag,
+        'maxx': maxx,
+        'minx': minx,
+        'maxy': maxy,
+        'miny': miny,
+        'metadata': metadata,
+        'res_x': res_x,
+        'res_y': res_y,
+        'rows': rows,
+        'cols': cols
+    })
+
+    hdf5File = bio.getvalue()
+
+    # upload hdf5 file to firebase storage
+    path = storage.child(f"/s104/hdf5/{file_name}.h5")
+    path.put(hdf5File)
+    url = storage.child(f"s104/hdf5/{file_name}.h5").get_url(None)
+
+    input = jsonable_encoder(input)
+    input["hdf5Uri"] = url
 
     # generate id(s)
     uid4 = uuid4()
     uuid_str = str(uid4)
 
-    input = jsonable_encoder(input)
-    input["hdf5Uri"] = ""
-
     # insert new s104 to mongodb
-    new_s1104 = request.app.database["s104"].insert_one({
+    new_s104 = request.app.database["s104"].insert_one({
         "_id": uuid_str,
         "hdf5Uri": input["hdf5Uri"],
         "geojsonUri": "",
@@ -37,7 +114,7 @@ def create_s104(request: Request, input: S104Product = Body(...)):
     })
 
     created_s104 = request.app.database["s104"].find_one(
-        {"_id": new_s1104.inserted_id}
+        {"_id": new_s104.inserted_id}
     )
 
     return created_s104
---
+++ s100/utils/tiff_hdf5_s104.py
@@ -0,0 +1,162 @@
+from h5py import File, special_dtype
+import numpy
+from typing import Dict, Union
+from osgeo import osr
+
+
+def tiff_hdf5_s104(bio, bio_param: Dict[str, Union[str, int]]):
+    dataset_deg = bio_param['dataset_deg']
+    dataset_mag = bio_param['dataset_mag']
+    maxx: str = bio_param['maxx']
+    minx: str = bio_param['minx']
+    maxy: str = bio_param['maxy']
+    miny: str = bio_param['miny']
+    metadata: Dict[str, Union[str, bool, int]] = bio_param['metadata']
+    # vertical_datum_dt_type: int = bio_param['vertical_datum_dt_type']
+    # vertical_datum_dt: numpy.dtype = bio_param['vertical_datum_dt']
+    # sequencing_rule_type_dt_type: int = bio_param['sequencing_rule_type_dt_type']
+    # sequencing_rule_type_dt: numpy.dtype = bio_param['sequencing_rule_type_dt']
+    # interpolation_type_dt_type: int = bio_param['interpolation_type_dt_type']
+    # interpolation_type_dt: numpy.dtype = bio_param['interpolation_type_dt']
+    # data_coding_format_dt_type: int = bio_param['data_coding_format_dt_type']
+    # data_coding_format_dt: numpy.dtype = bio_param['data_coding_format_dt']
+    # common_point_rule_dt_type: int = bio_param['common_point_rule_dt_type']
+    # common_point_rule_dt: numpy.dtype = bio_param['common_point_rule_dt']
+    res_x: str = bio_param['res_x']
+    res_y: str = bio_param['res_y']
+    rows: int = bio_param['rows']
+    cols: int = bio_param['cols']
+
+    # Use band 1 from deg to get the shape etc.
+    deg_band_1 = dataset_deg.GetRasterBand(1)
+    deg_grid_1 = deg_band_1.ReadAsArray()
+
+    # group all band in array
+    num_deg_bands = dataset_deg.RasterCount
+    deg_band_arrays = []
+
+    for deg_band_number in range(1, num_deg_bands + 1):
+        deg_band = dataset_deg.GetRasterBand(deg_band_number)
+        deg_band_array = deg_band.ReadAsArray()
+        deg_band_arrays.append(deg_band_array)
+
+    num_mag_bands = dataset_mag.RasterCount
+    mag_band_arrays = []
+
+    for mag_band_number in range(1, num_mag_bands + 1):
+        mag_band = dataset_mag.GetRasterBand(mag_band_number)
+        mag_band_array = mag_band.ReadAsArray()
+        mag_band_arrays.append(mag_band_array)
+
+    with File(bio, 'w') as f:
+        # initiate dataset structure
+        surf = f.create_group('/SurfaceCurrent')
+        surf_01 = f.create_group('/SurfaceCurrent/SurfaceCurrent.01')
+
+        mag_array = mag_band_arrays
+        deg_array = deg_band_arrays
+        # time_points = ['2022-09-29 16:00:00Z',
+        #                '2022-09-29 20:00:00Z', '2022-09-29 16:00:00Z']
+
+        for idx, (mag_grid, deg_grid) in enumerate(zip(mag_array, deg_array), start=1):
+            group_path = f'/SurfaceCurrent/SurfaceCurrent.01/Group_{idx:03}'
+            surf_group_object = surf_01.create_group(group_path)
+            grid = surf_group_object.create_dataset(
+                'values', dtype=[('surfaceCurrentSpeed', '<f4'), ('surfaceCurrentDirection', '<f4')], shape=deg_grid.shape
+            )
+            grid['surfaceCurrentSpeed'] = mag_grid
+            grid['surfaceCurrentDirection'] = deg_grid
+            # surf_group_object.attrs['timePoint'] = numpy.string_(time_point)
+
+        Group_F = f.create_group('Group_F')
+        Group_F = f['/Group_F']
+
+        # initiate root attrs.
+        f.attrs['productSpecification'] = "INT.IHO.S-111.1.0.1"
+        f.attrs['eastBoundLongitude'] = maxx
+        f.attrs['westBoundLongitude'] = minx
+        f.attrs['southBoundLatitude'] = miny
+        f.attrs['northBoundLatitude'] = maxy
+        f.attrs['depthTypeIndex'] = 2
+        f.attrs['surfaceCurrentDepth'] = 0.0
+
+        # these names are taken from the S100/S102 attribute names
+        if "horizontalDatumReference" in metadata:
+            f.attrs['horizontalDatumReference'] = metadata.get(
+                "horizontalDatumReference", "EPSG")
+        if "horizontalDatumValue" in metadata:
+            source_epsg = int(metadata.get("horizontalDatumValue", 0))
+            f.attrs['horizontalDatumValue'] = source_epsg
+        if "epoch" in metadata:
+            # e.g. "G1762"  this is the 2013-10-16 WGS84 used by CRS
+            f.attrs['epoch'] = metadata.get("epoch", "")
+        if "geographicIdentifier" in metadata:
+            f.attrs['geographicIdentifier'] = metadata.get(
+                "geographicIdentifier", "")
+        if "issueDate" in metadata:
+            f.attrs['issueDate'] = metadata.get("issueDate", "")
+        if "metadata" in metadata:
+            f.attrs['metadata'] = metadata.get("metadata", "")
+        if "issueTime" in metadata:
+            f.attrs['issueTime'] = metadata.get("issueTime", "")
+
+        # initiate surfCurrent attribute
+        srs = osr.SpatialReference()
+        srs.ImportFromEPSG(source_epsg)
+        if srs.IsProjected():
+            # ["Northing", "Easting"]  # row major instead of
+            axes = ["Easting", "Northing"]
+        else:
+            # ["Latitude", "Longitude"]  # row major instead of
+            axes = ["Longitude", "Latitude"]
+
+        axis_names = surf.create_dataset('axisNames', data=axes)
+
+        surf.attrs['verticalUncertainty'] = -1.0
+        surf.attrs['timeUncertainty'] = -1.0
+        surf.attrs.create('sequencingRule.type', data=1)
+        surf.attrs['numInstances'] = 1
+        surf.attrs['horizontalPositionUncertainty'] = -1.0
+        surf.attrs['dimension'] = 2
+        surf.attrs['sequencingRule.scanDirection'] = ", ".join(axes)
+        surf.attrs.create('interpolationType', data=1)
+        surf.attrs.create('dataCodingFormat', data=2)
+        surf.attrs.create('commonPointRule', data=2)
+
+        # initiate BathCov.nn attribute
+        surf_01.attrs['eastBoundLongitude'] = maxx
+        surf_01.attrs['westBoundLongitude'] = minx
+        surf_01.attrs['southBoundLatitude'] = miny
+        surf_01.attrs['northBoundLatitude'] = maxy
+
+        surf_01.attrs['numberOfTimes'] = 3
+        surf_01.attrs['timeRecordInterval'] = 3600
+        surf_01.attrs['dateTimeOfFirstRecord'] = '2022-09-29 16:00:00Z'
+        surf_01.attrs['dateTimeOfLastRecord'] = '2022-09-30 00:00:00Z'
+
+        surf_01.attrs['gridSpacingLatitudinal'] = abs(res_y)
+        surf_01.attrs['gridSpacingLongitudinal'] = abs(res_x)
+        surf_01.attrs['gridOriginLatitude'] = miny
+        surf_01.attrs['gridOriginLongitude'] = minx
+
+        surf_01.attrs['numGRP'] = 3
+        surf_01.attrs['numPointsLatitudinal'] = rows
+        surf_01.attrs['numPointsLongitudinal'] = cols
+        surf_01.attrs['startSequence'] = "0,0"
+
+        # fill Group_F
+        dt_dtype = special_dtype(vlen=str)
+        dt = numpy.dtype([('code', dt_dtype), ('name', dt_dtype), ('uom.name', dt_dtype), ('fillValue', dt_dtype),
+                          ('datatype', dt_dtype), ('lower', dt_dtype), ('upper', dt_dtype), ('closure', dt_dtype)])
+
+        scurrent = Group_F.create_dataset(
+            'SurfaceCurrent', shape=(2,), dtype=dt)
+
+        scurrent[0] = ('surfaceCurrentSpeed', 'Surface current speed',
+                       'knots', '-9999.0', 'H5T_FLOAT', '0.0', '', 'geSemiInterval')
+        scurrent[1] = ('surfaceCurrentDirection', 'Surface current direction',
+                       'arc-degrees', '-9999.0', 'H5T_FLOAT', '-0.0', '360', 'geLtInterval')
+
+        fcode = Group_F.create_dataset(
+            'featureCode', shape=(1,), dtype=dt_dtype)
+        fcode[0] = ('SurfaceCurrent')
